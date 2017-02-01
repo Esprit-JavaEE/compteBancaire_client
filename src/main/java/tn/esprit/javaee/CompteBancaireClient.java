6ea1aa3d5aa23b16625857eda6b062d2e4e7572c
Walid-YAICH <wyaich@outlook.fr>
2017-02-01 17:09:48 +0100

    Ce client va appeler un EJB remote déployé sur Wildfly 9

---
+++ src/main/java/tn/esprit/javaee/CompteBancaireClient.java
@@ -0,0 +1,28 @@
+package tn.esprit.javaee;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import javaee.ejb.CompteBancaireRemote;
+
+
+
+public class CompteBancaireClient {
+
+	public static void main(String[] args) throws  InterruptedException, NamingException {
+		
+		//Deux executions signifie 2 clients differents !
+		String jndiName="ejb_stateless_stateful/CompteBancaire!javaee.ejb.CompteBancaireRemote";
+
+		Context context = new InitialContext();
+		CompteBancaireRemote proxy=(CompteBancaireRemote) context.lookup(jndiName);
+		System.out.println(proxy.versement("Walid_YAICH", 20)); //Le client demande un versement
+		System.out.println(proxy.retrait("Walid_YAICH", 10));    //Le client demande un retrait
+		System.out.println(proxy.versement("Walid_YAICH", 10));  //Le client demande un versement
+		System.out.println(proxy.retrait("Walid_YAICH", 10));    //Le client demande un retrait
+
+		//EXIT
+	}
+}
+
